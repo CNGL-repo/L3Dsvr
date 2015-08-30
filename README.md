ca9f2ccc5c9f3fad5a04ba0b3d5b4bf05cefd2f4
calvodea <calvodea@tcd.ie>
2015-08-30 23:38:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# L3Dsvr Instructions and Methods
+
+## Description
+Core of L3Data server in the FALCON project (http://falcon-project.eu/). 
+
+## Methods
+
+### addCSVFile
+Receives the URL of a CSV file and copies it into the relevant folder in the L3Dsvr server
+Method: POST addCSVFile
+Input: sourceURL, custId, projId, sourceLang, targetLang
+Response: outputURL
+
+
+## How to run locally
+1. Import the project in Eclipse JEE Mars from https://github.com/CNGL-repo/L3Dsvr.git
+2. On L3DsvrServices.java, comment the [PRODUCTION] lines and discomment the [DEVELOPMENT] ones
+3. Update the username, replacing "calvodea" by the correct one
+4. Run As -> Run on Server. Choose server Tomcat v8.0
+5. From a terminal session, type the following example
+   curl -X POST --data "sourceURL=https://docs.shopify.com/manual/your-store/products/product_template.csv&custId=1234&projId=5678&sourceLang=es&targetLang=en" http://localhost:8080/L3Dsvr/api/addCSVFile
+6. It will return something like {"outputURL": "/home/calvodea/workspace/L3Dsvr/files/1234/5678/es/en/4.csv"}
+7. If you open /home/calvodea/workspace/L3Dsvr/files/1234/5678/es/en/4.csv you will see the csv file
+
+## How to run in production
+1. From a terminal session, type the following example
+   curl -X POST --data "sourceURL=https://docs.shopify.com/manual/your-store/products/product_template.csv&custId=1234&projId=5678&sourceLang=es&targetLang=en" http://l3dsvr.peepp.ie/api/addCSVFile
+2. It will return something like {"outputURL": "http://l3dsvr.peep.ie/files/1234/5678/es/en/4.csv"}
+3. If you open http://l3dsvr.peep.ie/files/1234/5678/es/en/4.csv you will see the optimal path file
+
+## How to deploy to a production server
+1. Install Tomcat 8 in the production server (path /opt/tomcat8)
+2. On L3DsvrServices.java, comment the [PRODUCTION] lines and discomment the [DEVELOPMENT] ones
+3. Update the productions URLS if this is not L3Dsvr.peep.ie server. Update the basePath if necessary.
+4. From Eclipse, select the project and right-click Export -> Web -> WAR file
+5. Upload the new L3Dsvr.war file to the server /opt/tomcat8/webapps. It will automatically extract its content into a L3Dsvr folder
+6. Restart the Tomcat server: sudo sh /opt/tomcat8/bin/start.sh
+7. Follow the instructions in "How to run in production" to test if it is working properly
---
+++ src/l3dsvr/com/webservices/L3DsvrServices.java
@@ -0,0 +1,172 @@
+package l3dsvr.com.webservices;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.joox.Match;
+import org.w3c.dom.DOMException;
+import org.xml.sax.SAXException;
+
+import static org.joox.JOOX.$;
+
+@Path("/")
+public class L3DsvrServices {
+	
+/* Comment or discomment the lines below depending on the environment, and update the username ("calvodea") */
+//	String basePath = "/var/www/webservices/L3Dsvr/"; // [PRODUCTION] Path to server 
+//	String resultsBaseURL = "http://l3dsvr.peep.ie/files/"; // [PRODUCTION] URL to CSV files
+	String basePath = "/home/calvodea/workspace/L3Dsvr/"; // [DEVELOPMENT] Path to server 
+	String resultsBaseURL = basePath + "files/"; // [DEVELOPMENT] URL to CSV files
+	
+	@POST
+	@Path("/addCSVFile")
+	@Produces(MediaType.APPLICATION_JSON)
+	/* Receives the URL of a CSV file, the customerId, projectId, source language and target language
+	 * and copies it into the folder files/customerId/projectId/sourceLanguage/targetLanguage/ in the L3Dsvr server */
+	public Response addCSVFileService(
+			@FormParam("sourceURL") String sourceFileURL, // e.g. "https://docs.shopify.com/manual/your-store/products/product_template.csv"
+			@FormParam("custId") String customerId, // e.g. "1234"
+			@FormParam("projId") String projectId, // e.g. "5678"
+			@FormParam("sourceLang") String sourceLanguage, // e.g. "pl"
+			@FormParam("targetLang") String targetLanguage) // e.g. "en"
+					throws IOException, InterruptedException, DOMException, SAXException { 
+		
+		// If URL is not accessible, return error
+		if (isURLAccessible(sourceFileURL) == false) {
+			String unavailableFileResponse = "{\"error\": \"Input file not available\"}";			
+			return Response.ok(unavailableFileResponse, MediaType.APPLICATION_JSON).build();
+		}
+		
+		// If URL is not in the correct format, return error
+		if (isFileInCorrectFormat(sourceFileURL) == false) {
+			String wrongFormatFileResponse = "{\"error\": \"Wrong format in input file. Expecting .csv extension.\"}";			
+			return Response.ok(wrongFormatFileResponse, MediaType.APPLICATION_JSON).build();
+		}
+		
+		// Generate a new processingId for the optimal path generation
+		String processingId = getNextProcessingId();
+		
+		String localFilePath = copyFileToLocalhost(sourceFileURL, customerId, projectId, sourceLanguage, targetLanguage, processingId);
+				
+		String jsonResponse = "{\"outputURL\": \"" + localFilePath + "\"}";
+		
+		return Response.ok(jsonResponse, MediaType.APPLICATION_JSON).build();
+	}
+
+
+	/* Checks that the provided URL is accessible */
+	private boolean isURLAccessible(String xliffFileURL)
+			throws MalformedURLException, IOException, ProtocolException {
+
+		URL xliffURL = new URL(xliffFileURL);
+		HttpURLConnection testConnection = (HttpURLConnection)xliffURL.openConnection();
+		testConnection.setRequestMethod("HEAD"); 
+		int code = testConnection.getResponseCode();
+		
+		if (code == HttpURLConnection.HTTP_OK) // OK, accessible
+			return true;
+		else // Not accessible
+			return false;
+	}
+	
+	
+	/* Checks that if the file is in the correct CSV format */
+	private boolean isFileInCorrectFormat(String sourceFileURL)
+			throws MalformedURLException, IOException, ProtocolException {
+
+		// Get the file extension
+		String fileExtension = sourceFileURL.substring(sourceFileURL.length() - 3, sourceFileURL.length());
+		
+		if (fileExtension.equalsIgnoreCase("csv")) // OK, correct format
+			return true;
+		else // Wrong format
+			return false;
+	}
+	
+	
+	// Read from sequence.txt the number, increment it + 1, overwrite the file and return it
+	private String getNextProcessingId() {
+		
+		// Read original number
+		File file = new File(basePath + "sequence.txt");
+		BufferedReader reader = null;
+		String currentProcessingId = null;
+		
+		try {
+		    reader = new BufferedReader(new FileReader(file));
+		    currentProcessingId = reader.readLine(); // Read number from file
+		} catch (FileNotFoundException e) {
+		    e.printStackTrace();
+		} catch (IOException e) {
+		    e.printStackTrace();
+		} finally {
+		    try {
+		        if (reader != null) {
+		        	int nextProcessingId = Integer.parseInt(currentProcessingId) + 1;
+		            reader.close(); // Close read file
+		        	BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+		        	writer.write(Integer.toString(nextProcessingId));
+		        	writer.close(); // Close write file
+		        }
+		    } catch (IOException e) {
+		    }
+		}
+		
+		return currentProcessingId;
+	}
+	
+	
+	/* Make an exact copy of the remote CSV file in files/customerId/projectId/sourceLanguage/targetLanguage/processingId.csv
+	 * in the L3Dsvr server, returning the path */
+	private String copyFileToLocalhost(String remoteFileURL, String customerId, String projectId,
+			String sourceLanguage, String targetLanguage, String processingId) throws IOException {
+		
+		String localFilePath = resultsBaseURL + customerId + "/" + projectId + "/" +
+				sourceLanguage + "/" + targetLanguage;
+		
+		String localFileURL = localFilePath + "/" + processingId + ".csv";
+		
+		URL inputURL = new URL(remoteFileURL);
+		InputStream inputCSVFile = inputURL.openStream();
+		
+	    File localFileDirectory = new File(localFilePath);
+	    // Check if folders do not exist, and create them if so
+	    if(localFileDirectory.exists() == false) {
+	    	localFileDirectory.mkdirs();
+	    }
+		
+		FileOutputStream localCSVFile = new FileOutputStream(localFileURL);
+		final int BUF_SIZE = 1 << 8;
+		byte[] buffer = new byte[BUF_SIZE];
+		int bytesRead = -1;
+		while((bytesRead = inputCSVFile.read(buffer)) > -1) {
+			localCSVFile.write(buffer, 0, bytesRead);
+		}
+		
+		inputCSVFile.close();
+		localCSVFile.close();
+		
+		return localFileURL;
+	}
+}
